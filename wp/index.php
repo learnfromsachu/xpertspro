3d96d8170dd65c541cfe4c5b5f67c7005f6f9059
Ankathwar Sachin <95225050+learnfromsachu@users.noreply.github.com>
2023-08-19 15:32:13 +0530

    Update index.php

--- wp/index.php
+++ wp/index.php
@@ -1,4 +1,11 @@
 <?php
+<?php
+  $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+  $mysqli->close();
+?>
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
